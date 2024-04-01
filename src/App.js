69a65649d27aae643898e72e948e8275f0a0538a
Risini Piyathma <risinipiyathma1998@gmail.com>
2024-04-01 13:13:42 +0530

    perf(pencil): remove graphiteWidth option
    BREAKING CHANGE: The graphiteWidth option has been removed.
    The default graphite width of 10mm is always used for performance reasons.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. hiiisdeessaaddhh
+          Edit <code>src/App.js</code> and save to reload. hiiisdeessaaddhhaa
         </p>
         <a
           className="App-link"
