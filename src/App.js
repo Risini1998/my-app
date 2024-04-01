e8a103804ed653626c07cfb851dc3fab017a0da2
Risini Piyathma <risinipiyathma1998@gmail.com>
2024-04-01 11:38:03 +0530

    perf: fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. hiii
+          Edit <code>src/App.js</code> and save to reload. hiiis
         </p>
         <a
           className="App-link"
